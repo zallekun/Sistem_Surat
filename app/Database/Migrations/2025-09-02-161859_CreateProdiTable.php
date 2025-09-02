38502eef4987c02f5feb6c92a38786679e5dce4b
Rezal Suryadi Putra <suryadirezal@gmail.com>
2025-09-02 23:24:17 +0700

    Add all project files

---
+++ app/Database/Migrations/2025-09-02-161859_CreateProdiTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProdiTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
